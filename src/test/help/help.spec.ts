c3da8ad5cacabd968ce0aa5a26ce75c14d763aee
syler <hileo77@gmail.com>
2020-11-09 18:47:33 +0100

    add hel p test

---
+++ src/test/help/help.spec.ts
@@ -0,0 +1,20 @@
+import JestStoreLog from 'jest-store-log';
+import { removeNodeStyles } from 'suf-log';
+import { run } from '../../run';
+
+test('cli: help', async () => {
+  process.chdir(__dirname);
+  process.argv = ['', '', 'h'];
+  const log = new JestStoreLog();
+  await run();
+
+  expect(removeNodeStyles(log.logs[0]).replace(/ +/g, '-')).toEqual(`
+-
+-INFO-All-arguments-can-start-with---or---,-but-i-would-recommend-to-just-use-letters.-
+-a-|-all-Calls-all-modules.-
+-b-|-badges-Calls-the-badges-module.-
+-t-|-ts-|-d.ts-|-docs-Calls-the-tsDoc-module.-
+-l-|-licence-Calls-the-license-module.-
+-h-|-help-Displays-this-Message.-
+-`);
+});
